d3e2d5eeb7cf9c313492c5beecbeba830c7ba16f
Tamara Burcak <tamarabolmanac@gmail.com>
2026-05-29 16:59:41 +0200

    NP

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       "http://localhost:3001",
       "http://127.0.0.1:3001",
       "https://localhost",
+      "http://localhost",          # Capacitor Android WebView (starije verzije)
+      "capacitor://localhost",     # Capacitor Android/iOS native bundle
+      "ionic://localhost",         # Ionic kompatibilnost
       "https://hajki.com",
       "https://www.hajki.com"
     )
